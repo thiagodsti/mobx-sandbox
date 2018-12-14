4c8cb257cf8c3affb5b91eaac0a777f07b655cb2
Thiago Diniz da Silveira <thiago.silveira@0x.se>
2018-12-14 14:00:52 +0100

    Trying to fix storybook with mobx

--- src/containers/loginContainer.stories.js
+++ src/containers/loginContainer.stories.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import LoginContainer from './loginContainer';
+import { Provider } from 'mobx-react';
 
 const store = {
-  getState: () => {
-    return {
-    };
-  },
+  isAuthenticated: false
 };
 
 storiesOf('LoginContainer', module)
-  .addDecorator(story => <Provider store={store}>story()</Provider>)
+  .addDecorator(story => <Provider store={store}>{story}</Provider>)
   .add('with text', () => (
-    <LoginContainer>Hello Button</LoginContainer>
+    <LoginContainer/>
   ));
