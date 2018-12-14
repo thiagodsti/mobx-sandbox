cb10a2141db6fc7253b5b9039830b57dc06929e7
Thiago Diniz da Silveira <thiago.silveira@0x.se>
2018-12-14 14:57:45 +0100

    Fixed test to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'mobx-react';
+import { MemoryRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider authStore={{}} >
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
